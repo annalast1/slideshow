9bdfb58fe52ad93b695cbddff90f3719e5edd14c
annalast1 <166384111+annalast1@users.noreply.github.com>
2025-07-16 13:38:46 +0100

    Completed slideshow

---
+++ js-file.js
@@ -0,0 +1,54 @@
+// Set current slide
+let currentSlide = 0;
+
+// make prev + next buttons + dots functional
+(function() {
+    const prev = document.getElementById('prev');
+    prev.addEventListener("click", () => {
+        changeSlide('back');
+    })
+    const next = document.getElementById('next');
+    next.addEventListener("click", () => { 
+    changeSlide('forward');
+    })
+    const dots = document.getElementsByClassName('dot');
+    const l = dots.length;
+    for (let i = 0; i < l; i++) {
+        dots[i].addEventListener("click", () => {
+            const slides = document.getElementsByClassName('slide');
+            slides[currentSlide].removeAttribute('data-active', slides[currentSlide]);
+            currentSlide = i;
+            displaySlide();
+        })
+    }      
+})()
+
+// function to move current slide value
+function changeSlide(direction) {
+    // remove image currently showing
+    const slides = document.getElementsByClassName('slide');
+    slides[currentSlide].removeAttribute('data-active', slides[currentSlide]);
+    switch (direction) {
+        case 'back':
+            currentSlide -= 1;
+            break;
+        case 'forward': 
+            currentSlide += 1;
+            break;
+        default:
+            console.log("changeSlide error");
+    }
+    displaySlide();
+}
+
+function displaySlide() {
+    const slides = document.getElementsByClassName('slide');
+    const slidesLength = slides.length;
+    // display next slide
+    if (currentSlide >= slidesLength) {
+        currentSlide = 0;
+    } else if (currentSlide < 0) {
+        currentSlide = slidesLength - 1;
+    }
+    slides[currentSlide].setAttribute('data-active', slides[currentSlide]);
+}
